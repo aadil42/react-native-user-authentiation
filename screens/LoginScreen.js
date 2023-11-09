4d99977a99603ec9312cd91904df06122d16d6d5
aadil42 <adilkelawala@gmail.com>
2023-11-09 14:07:08 +0530

    login user. display error and success msg if logged in.

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,36 @@
+import { useState } from 'react';
+import { Alert } from 'react-native';
 import AuthContent from '../components/Auth/AuthContent';
+import { loginUser } from '../utils/auth';
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const logUser = async ({email, password}) => {
+      try {
+        setIsLoading(true);
+        const response = await loginUser(email, password);  
+        setIsLoading(false);
+        Alert.alert("Success!", "Welcom", [
+          {
+            text: "Ok",
+            onPress: () => setIsLoading(false)
+          }
+        ]);
+        return response;  
+      }catch (error) {
+        
+        Alert.alert("Invalid credentials", "Email or password or both are incorrect", [
+          {
+            text: "Try again",
+            onPress: () => setIsLoading(false)
+          }
+        ]);
+      }
+  }
+
+  return <AuthContent isLoading={isLoading} onAuthenticate={logUser} isLogin />;
 }
 
 export default LoginScreen;
--- utils/auth.js
+++ utils/auth.js
@@ -2,12 +2,20 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyB2ZvLUuQHRGyacM2xHnc4ZqN_QVaZkj98";
 
-export const createUser = async (email, password) => {
-    const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+export const authenticateUser = async (email, password, mode) => {
+    const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`,
     {   
         email: email,
         password: password,
         returnSecureToken: true
     });
     return response;
+}
+
+export const loginUser = async (email, password) => {
+    return await authenticateUser(email, password, "signInWithPassword");
+}
+
+export const createUser = async (email, password) => {
+  return await authenticateUser(email, password, "signUp");
 }
